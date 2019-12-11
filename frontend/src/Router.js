7738a18568ea8884f2004d07656663125e909efd
RicardoSalas17 <ricardosalas.1716@gmail.com>
2019-12-11 11:16:57 -0600

    auth conectado

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -2,11 +2,18 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/home/Home';
 import NotFound from './components/404/NotFound.js';
+import Navbar from './components/styled-components/Navbar'
+import SignupContainer from './components/Signup/SignupContainer'
+import LoginContainer from './components/Login/LoginContainer'
+
 
 const Router = () => (
   <BrowserRouter>
+  <Navbar />
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path="/signup" component={SignupContainer} />
+      <Route exact path="/login" component={LoginContainer} />
       <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
---
+++ frontend/src/components/Login/LoginContainer.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import { Form, Input, Icon, Button } from 'antd'
+import { MyContext } from '../../context'
+
+export default class LoginContainer extends React.Component {
+  componentDidMount() {
+    console.log(this.context)
+    if (this.context.loggedUser) {
+      return this.props.history.push('/events')
+    }
+  }
+
+  render() {
+    return (
+      <MyContext.Consumer>
+        {context => (
+          <Form
+            onSubmit={e => {
+              context.handleLogin(e, () => {
+                this.props.history.push('/events')
+              })
+            }}
+          >
+            <Form.Item>
+              <Input
+                name="email"
+                prefix={
+                  <Icon type="mail" style={{ color: 'rgba(0,0,0,.25)' }} />
+                }
+                placeholder="Email"
+                type="email"
+                value={context.loginForm.email}
+                onChange={e => context.handleInput(e, 'loginForm')}
+              />
+            </Form.Item>
+
+            <Form.Item>
+              <Input
+                name="password"
+                type="password"
+                prefix={
+                  <Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />
+                }
+                placeholder="Password"
+                value={context.loginForm.password}
+                onChange={e => context.handleInput(e, 'loginForm')}
+              />
+            </Form.Item>
+
+            <Form.Item>
+              <Button type="primary" htmlType="submit">
+                Login
+              </Button>
+            </Form.Item>
+          </Form>
+        )}
+      </MyContext.Consumer>
+    )
+  }
+}
+
+LoginContainer.contextType = MyContext
